71d26786f71a62373ae9d72aa35542f22780840f
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2022-05-29 11:43:07 +0100

    removed bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask
 from flask_pymongo import PyMongo
-from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb+srv://danieliheonu:QjWwd3n38hErGIMw@cluster0.nl5pe.mongodb.net/sloovi"
 mongo = PyMongo(app)
-bcrypt = Bcrypt(app)
 
 from app import routes
